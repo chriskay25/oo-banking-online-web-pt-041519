bdc5e71d4853ae700e7d26f9d7c888f11ab75270
Chris Kay <chriskay25@gmail.com>
2019-05-29 18:45:19 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,6 +17,9 @@ class BankAccount
     "Your balance is $#{@balance}."
   end 
   
+  def valid?
+    self.status == "open" && self.balance > 0 
+  end 
   
   def close_account
     self.status = "closed"
