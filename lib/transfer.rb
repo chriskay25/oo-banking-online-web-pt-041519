817509090ab5360a5beeac75a205ee69fbfdd1cf
Chris Kay <chriskay25@gmail.com>
2019-05-29 18:48:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,11 @@
 class Transfer
   
-  def initialize(name,)
+  attr_accessor :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver 
+    @amount = amount 
+  end 
   
 end
